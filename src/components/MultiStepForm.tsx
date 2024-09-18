c6743c844c5a445b51bf71555526fee7cb5765b6
Kier <kieransmozer@gmail.com>
2024-09-18 16:34:28 +0100

    Polishing navbar

--- src/components/MultiStepForm.tsx
+++ src/components/MultiStepForm.tsx
@@ -104,7 +104,11 @@ function MultiStepForm({
 
   const symptomOptions = [
     { value: 'nausea', label: 'Nausea' },
-    { value: 'headache', label: 'Headache' },
+    { value: 'light sensitivity', label: 'Light sensitivity' },
+    { value: 'fatigue', label: 'Fatigue' },
+    { value: 'neck pain', label: 'Neck pain' },
+    { value: 'irritability', label: 'Irritability' },
+    { value: 'vomiting', label: 'Vomiting' },
     { value: 'eye pain', label: 'Eye Pain' },
   ];
 
@@ -112,6 +116,10 @@ function MultiStepForm({
     { value: 'caffeine', label: 'Caffeine' },
     { value: 'stress', label: 'Stress' },
     { value: 'humidity', label: 'Humidity' },
+    { value: 'illness', label: 'Illness' },
+    { value: 'poor sleep', label: 'Poor sleep' },
+    { value: 'physical exertion', label: 'Physical exertion' },
+    { value: 'alcohol', label: 'Alcohol' },
   ];
 
   return (
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Button } from './ui/button';
+import { Cog6ToothIcon } from '@heroicons/react/24/outline';
 import MultiStepForm from './MultiStepForm';
 
 function Navbar() {
@@ -10,7 +11,7 @@ function Navbar() {
   return (
     <>
       <div className="w-1/6 h-full flex flex-col justify-between bg-gray-800 text-white p-4">
-        <div>
+        <div className="flex-grow">
           <h2 className="text-2xl text-left ml-1 mt-4">MigrainePal</h2>
           <Button className="mt-10 w-3/4 bg-foreground text-base">
             Dashboard
@@ -19,12 +20,18 @@ function Navbar() {
             Analytics
           </Button>
         </div>
-        <Button
-          onClick={openDialog}
-          className="mt-6 w-3/4 text-base mb-4 bg-[hsl(var(--button-primary))] hover:bg-[hsl(var(--button-primary-hover))]"
-        >
-          Log Migraine
-        </Button>
+        <div>
+          <Button
+            onClick={openDialog}
+            className="mt-6 w-3/4 text-lg mb-10 py-5 rounded-md bg-[hsl(var(--button-primary))] hover:bg-[hsl(var(--button-primary-hover))]"
+          >
+            Log Migraine
+          </Button>
+          <Button className="mt-6 w-1/2 py-0 text-base mb-2 bg-foreground rounded-sm">
+            <Cog6ToothIcon className="h-5 w-5 mr-3" />
+            Settings
+          </Button>
+        </div>
       </div>
       <MultiStepForm isOpen={isDialogOpen} onOpenChange={setIsDialogOpen} />
     </>
