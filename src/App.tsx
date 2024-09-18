91d9c1b17ff4ca14f8c4b256b4e3995a033d0faf
Kier <kieransmozer@gmail.com>
2024-09-18 09:44:42 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { Button } from './components/ui/button';
+
+function App() {
+  return (
+    <>
+      <h2>Migraine Pal</h2>
+      <Button>Hello World</Button>
+    </>
+  );
+}
+
+export default App;
