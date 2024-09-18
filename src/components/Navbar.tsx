7e2b127485a65bfba984e64196409cb566167ffa
Kier <kieransmozer@gmail.com>
2024-09-18 17:22:59 +0100

    Finishing navbar polishing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import { Button } from './ui/button';
-import { Cog6ToothIcon, Squares2X2Icon } from '@heroicons/react/24/outline';
 import {
+  Cog6ToothIcon,
+  Squares2X2Icon,
   ArrowLeftStartOnRectangleIcon,
-  ChartBarIcon,
   ChartBarSquareIcon,
 } from '@heroicons/react/24/outline';
 
@@ -17,13 +17,13 @@ function Navbar() {
   return (
     <>
       <div className="w-1/6 h-full flex flex-col justify-between bg-gray-800 text-white p-4">
-        <div className="flex-grow">
-          <h2 className="text-4xl text-left ml-1 mt-4">MigrainePal</h2>
-          <Button className="mt-10 w-8/12 bg-foreground text-base flex items-center justify-start gap-2 pl-4">
+        <h2 className="text-4xl text-left mt-4">MigrainePal</h2>
+        <div className="flex-grow flex flex-col items-center">
+          <Button className="mt-10 w-10/12 bg-foreground text-base flex items-center justify-start gap-2 pl-4">
             <Squares2X2Icon className="h-5 w-5" />
             <div className="flex-1 text-center">Dashboard</div>
           </Button>
-          <Button className="mt-6 w-8/12 bg-foreground text-base flex items-center justify-start gap-2 pl-4">
+          <Button className="mt-6 w-10/12 bg-foreground text-base flex items-center justify-start gap-2 pl-4">
             <ChartBarSquareIcon className="h-5 w-5" />
             <div className="flex-1 text-center">Analytics</div>
           </Button>
@@ -35,11 +35,11 @@ function Navbar() {
           >
             Log Migraine
           </Button>
-          <Button className="mt-10 mb-5 w-full text-base bg-foreground rounded-sm flex items-center justify-start gap-2 pl-4">
+          <Button className="mt-10 mb-3 w-1/2 text-base bg-foreground rounded-sm flex items-center justify-start gap-2 pl-4 ml-4">
             <Cog6ToothIcon className="h-5 w-5" />
             <div className="flex-1 text-center">Settings</div>
           </Button>
-          <Button className="mt-6 py-0 text-base mb-10 w-full bg-foreground rounded-sm flex items-center justify-start gap-2 pl-4">
+          <Button className="text-base mb-10 w-1/2 bg-foreground rounded-sm flex items-center justify-start gap-2 pl-4 ml-4">
             <ArrowLeftStartOnRectangleIcon className="h-5 w-5" />
             <div className="flex-1 text-center">Log out</div>
           </Button>
