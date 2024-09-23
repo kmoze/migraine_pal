4b5049e4c5fe0c39dfe303cce8426e4a28ec105a
Kier <kieransmozer@gmail.com>
2024-09-23 14:39:34 +0100

    Building out migraine deletion functionality

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,9 @@ interface Migraine {
 
 function App() {
   const [migraines, setMigraines] = useState<Migraine[]>([]);
+  const [mostRecentMigraine, setMostRecentMigraine] = useState<Migraine | null>(
+    null
+  );
 
   useEffect(() => {
     getMigraines();
@@ -29,11 +32,42 @@ function App() {
 
   async function getMigraines() {
     const { data } = await supabase.from('migraine_logs').select();
-    setMigraines(data || []);
+    const sortedData =
+      data?.sort(
+        (a: Migraine, b: Migraine) =>
+          new Date(b.date).getTime() - new Date(a.date).getTime()
+      ) || [];
+    setMigraines(sortedData);
+    // setMigraines(data || []);
+    if (sortedData.length > 0) {
+      setMostRecentMigraine(sortedData[0]); // Set the most recent log
+    }
   }
+
+  // Experimenting with deletion function
+  async function deleteMostRecentMigraine() {
+    if (mostRecentMigraine) {
+      const { error } = await supabase
+        .from('migraine_logs')
+        .delete()
+        .eq('id', mostRecentMigraine.id);
+
+      if (!error) {
+        setMigraines((prev) =>
+          prev.filter((migraine) => migraine.id !== mostRecentMigraine.id)
+        );
+        setMostRecentMigraine(null); // Clear the most recent migraine after deletion
+      }
+    }
+  }
+
+  console.log(migraines);
 
   return (
     <>
+      <button onClick={deleteMostRecentMigraine} className="w-1/3">
+        Delete Most Recent Migraine
+      </button>
       <div className="flex h-screen">
         <Navbar getMigraines={getMigraines} />
         <Routes>
