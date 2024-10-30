49e26fa8e29a3ae95adf7d3763f0d15417a92e91
Kier <kieransmozer@gmail.com>
2024-10-30 21:30:36 +0000

    file cleanup

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -32,17 +32,6 @@ function Login() {
 
   return (
     <div className="bg-card-darkModeSecondary w-full flex">
-      {/* <div className="w-1/2 flex flex-col justify-center items-center">
-        <h2 className="text-4xl text-white font-custom">MigrainePal</h2>
-        <Link to={'/'}>
-          <Button className="relative text-lg bg-card-coolorsSecondary hover:bg-card-coolorsAccent dark:text-card-lightMode px-20 py-5 mt-5 group">
-            Login
-            <span className="absolute right-5 top-1/2 transform -translate-y-1/2 transition-transform duration-500 ease-in-out group-hover:translate-x-2">
-              <ArrowRight w-6 h-6 />
-            </span>
-          </Button>
-        </Link>
-      </div> */}
       <div className="max-w-md w-full space-y-8 p-8 bg-white rounded-lg shadow">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
@@ -87,7 +76,6 @@ function Login() {
               />
             </div>
           </div>
-
           <div>
             <Button
               type="submit"
@@ -99,16 +87,8 @@ function Login() {
                 <ArrowRight w-6 h-6 />
               </span>
             </Button>
-            {/* <button
-              type="submit"
-              disabled={loading}
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:bg-indigo-400"
-            >
-              {loading ? 'Signing in...' : 'Sign in'}
-            </button> */}
           </div>
         </form>
-
         <div className="text-sm text-center">
           <p className="text-gray-600">
             Don't have an account?{' '}
